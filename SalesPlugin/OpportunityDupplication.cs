8f4588eeeb0723c126d05246e9067c6b4fbe8f63
ADM_NhanLC <ADM_NhanLC@CV1SCHVND004>
2025-04-29 12:27:48 +0100

    Plugin:associate

--- SalesPlugin/OpportunityDupplication.cs
+++ SalesPlugin/OpportunityDupplication.cs
@@ -1,16 +1,23 @@
-﻿using Microsoft.Xrm.Sdk;
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.Xrm.Sdk;
 using Microsoft.Xrm.Sdk.Query;
-using System;
+using Microsoft.Xrm.Sdk.Messages;
+using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Client;
 
 namespace SalesPlugin
 {
+    /// <summary>
+    /// Plugin to duplicate an Opportunity record and related stakeholders
+    /// </summary>
     public class OpportunityDupplication : IPlugin
     {
         public void Execute(IServiceProvider serviceProvider)
         {
             // Extract the tracing service for use in debugging
             ITracingService tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
-            tracingService.Trace("Outside of the plugin");
 
             try
             {
@@ -27,8 +34,7 @@ namespace SalesPlugin
                 {
                     // Get the opportunity reference from the context
                     EntityReference opportunityRef = (EntityReference)context.InputParameters["Target"];
-
-                    tracingService.Trace($"{opportunityRef}");
+                    tracingService.Trace($"Starting duplication for Opportunity ID: {opportunityRef.Id}");
 
                     // Ensure we're working with an opportunity entity
                     if (opportunityRef.LogicalName.Equals("opportunity", StringComparison.OrdinalIgnoreCase))
@@ -57,8 +63,7 @@ namespace SalesPlugin
                             </fetch>";
 
                         EntityCollection results = service.RetrieveMultiple(new FetchExpression(fetchXml));
-                        Console.WriteLine("Result: ", results);
-                        tracingService.Trace($"Result: {results}");
+
                         if (results.Entities.Count > 0)
                         {
                             Entity originalOpportunity = results.Entities[0];
@@ -76,7 +81,8 @@ namespace SalesPlugin
                                     attribute.Key != "createdby" &&
                                     attribute.Key != "modifiedon" &&
                                     attribute.Key != "modifiedby" &&
-                                    attribute.Key != "statecode" && attribute.Key != "statuscode" &&
+                                    attribute.Key != "statecode" &&
+                                    attribute.Key != "statuscode" &&
                                     attribute.Key != "versionnumber")
                                 {
                                     duplicateOpportunity[attribute.Key] = attribute.Value;
@@ -90,6 +96,9 @@ namespace SalesPlugin
                             // Create the duplicate opportunity record
                             tracingService.Trace("Creating duplicate opportunity");
                             Guid duplicateOpportunityId = service.Create(duplicateOpportunity);
+
+                            // Clone the stakeholder relationships
+                            CloneOpportunityStakeholders(service, tracingService, opportunityRef.Id, duplicateOpportunityId);
 
                             // Set the output parameter with the ID of the new opportunity
                             context.OutputParameters["DuplicatedOpportunityId"] = duplicateOpportunityId;
@@ -118,7 +127,76 @@ namespace SalesPlugin
             catch (Exception ex)
             {
                 tracingService.Trace($"Exception: {ex.Message}");
-                throw new InvalidPluginExecutionException($"An error occurred in OpportunityDuplicatePlugin: {ex.Message}", ex);
+                throw new InvalidPluginExecutionException($"An error occurred in OpportunityDupplication: {ex.Message}", ex);
+            }
+        }
+
+
+        private void CloneOpportunityStakeholders(IOrganizationService service, ITracingService tracingService, Guid originalOpportunityId, Guid duplicateOpportunityId)
+        {
+            try
+            {
+                tracingService.Trace("Starting to clone stakeholder relationships");
+
+                // Fetch the stakeholder relationships for the original opportunity
+                string fetchXml = $@"
+                    <fetch version='1.0' output-format='xml-platform' mapping='logical' distinct='false'>
+                       <entity name=""nhan_stakeholder_opportunity"">
+                         <attribute name=""nhan_stakeholder_opportunityid"" />
+                         <attribute name=""nhan_cre97_stakeholder"" />
+                         <attribute name=""nhan_opportunity"" />
+                         <filter type='and'>
+                          <condition attribute='nhan_opportunity' operator='eq' value='{originalOpportunityId}' />
+                        </filter>
+                    </entity>
+                    </fetch>";
+
+                EntityCollection stakeholderRelationships = service.RetrieveMultiple(new FetchExpression(fetchXml));
+
+                tracingService.Trace($"Found {stakeholderRelationships.Entities.Count} stakeholder relationships to clone");
+
+                // Create new intersect records for each stakeholder with the duplicate opportunity
+                foreach (var relationship in stakeholderRelationships.Entities)
+                {
+                    // Get the stakeholder ID from the relationship
+                    EntityReference stakeholderRef = relationship.GetAttributeValue<EntityReference>("nhan_cre97_stakeholder");
+
+                    if (stakeholderRef != null)
+                    {
+                        tracingService.Trace($"Creating relationship for stakeholder ID: {stakeholderRef.Id}");
+
+                        // Create a new record in the intersect entity
+                        Entity newRelationship = new Entity("nhan_stakeholder_opportunity");
+
+                        // Set the references to both the stakeholder and the new opportunity
+                        newRelationship["nhan_cre97_stakeholder"] = stakeholderRef;
+                        newRelationship["nhan_opportunity"] = new EntityReference("opportunity", duplicateOpportunityId);
+
+                        // If there are any other attributes in the relationship that need to be copied
+                        // you can add them here by iterating through relationship.Attributes
+                        // Example:
+                        // foreach (var attribute in relationship.Attributes)
+                        // {
+                        //     if (attribute.Key != "nhan_stakeholder_opportunityid" && 
+                        //         attribute.Key != "nhan_cre97_stakeholder" && 
+                        //         attribute.Key != "nhan_opportunity")
+                        //     {
+                        //         newRelationship[attribute.Key] = attribute.Value;
+                        //     }
+                        // }
+
+                        // Create the new relationship record
+                        Guid newRelationshipId = service.Create(newRelationship);
+                        tracingService.Trace($"Created new stakeholder relationship record with ID: {newRelationshipId}");
+                    }
+                }
+
+                tracingService.Trace("Finished cloning stakeholder relationships");
+            }
+            catch (Exception ex)
+            {
+                tracingService.Trace($"Error cloning stakeholder relationships: {ex.Message}");
+                throw new InvalidPluginExecutionException($"Error cloning stakeholder relationships: {ex.Message}", ex);
             }
         }
     }
